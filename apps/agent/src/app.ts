fa0ad9636cf392bb7bd07a78d27914b6e8b75b4a
Gram Liu <gram.liu226@gmail.com>
2023-10-22 00:10:13 -0700

    Add dotenv

--- apps/agent/src/app.ts
+++ apps/agent/src/app.ts
@@ -1,6 +1,9 @@
 import Koa from "koa";
 import bodyParser from "koa-bodyparser";
 import router from "./routes/router";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = new Koa();
 app.use(bodyParser());
