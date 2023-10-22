f3a4bf179b0666cb239c8f69899d96a3a554ac28
Gram Liu <gram.liu226@gmail.com>
2023-10-22 00:12:43 -0700

    Register route

--- apps/agent/src/routes/router.ts
+++ apps/agent/src/routes/router.ts
@@ -1,5 +1,7 @@
 import Router from "@koa/router";
+import registerRepository from "./repository/register";
 
 const router = new Router();
+router.post("/repository", registerRepository)
 
 export default router;
