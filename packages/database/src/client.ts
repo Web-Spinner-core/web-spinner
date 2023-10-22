201e9a431fe5f5c9ca0e27c321990bd42e2a824a
Gram Liu <gram.liu226@gmail.com>
2023-10-22 01:57:44 -0700

    Add ID prefixer to database package

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,13 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-import { env } from "./env";
+export * from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["error"],
-  });
-
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-export * from "@prisma/client"
+export const prisma = new PrismaClient({
+  log: ["error"],
+});
