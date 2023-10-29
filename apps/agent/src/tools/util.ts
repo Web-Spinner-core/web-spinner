8650e4eaea50d9158c032df4ba9019d358873e5e
Gram Liu <gram.liu226@gmail.com>
2023-10-28 20:45:03 -0700

    Handle labelRecordWithSchema recursive case

--- apps/agent/src/tools/util.ts
+++ apps/agent/src/tools/util.ts
@@ -12,12 +12,17 @@ export function labelRecordWithSchema<T extends ToolSchema>(
   object: z.infer<T>,
   schema: T
 ) {
-  const labeledRepositoryAnalysis = {} as Record<string, any>;
+  const labeled = {} as Record<string, any>;
   const directoryKeys = Object.keys(schema.shape) as string[];
   for (const key of directoryKeys) {
-    labeledRepositoryAnalysis[key] = {
-      value: object[key],
-      description: schema.shape[key].description,
-    };
+    if (object[key] && typeof object[key] === "object") {
+      labeled[key] = labelRecordWithSchema(object[key], schema.shape[key]);
+    } else {
+      labeled[key] = {
+        value: object[key],
+        description: schema.shape[key].description,
+      };
+    }
   }
+  return labeled;
 }
