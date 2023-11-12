1b72dfe2d2d84df73452ced42143d2d9b20dd370
Gram Liu <gram.liu226@gmail.com>
2023-11-11 16:25:38 -0800

    Move error handler to middleware folder

--- apps/agent/src/app.ts
+++ apps/agent/src/app.ts
@@ -1,38 +1,13 @@
 import Koa from "koa";
 import bodyParser from "koa-bodyparser";
-import { ZodError } from "zod";
-import APIError from "./lib/api_error";
 import { githubWebhooks } from "./lib/github";
 import { registerWebhookListeners } from "./lib/github/webhooks";
 import router from "./routes";
+import errorHandler from "./routes/middleware/error_handler";
 
 const app = new Koa();
 app.use(bodyParser());
-
-// Set error handler
-app.use(async (ctx, next) => {
-  try {
-    await next();
-  } catch (err) {
-    if (err instanceof ZodError) {
-      console.error(err.issues);
-      // Argument type validation
-      ctx.status = 400;
-      ctx.body = {
-        errors: err.issues,
-        message: err.message,
-      };
-    } else {
-      const typedError = err as APIError;
-      console.error(err);
-      console.error(typedError?.message ?? typedError?.type ?? typedError);
-      ctx.status = typedError?.statusCode ?? 500;
-      ctx.body = {
-        message: typedError?.message ?? typedError?.type ?? "An error occured",
-      };
-    }
-  }
-});
+app.use(errorHandler);
 
 // Register GitHub webhook listeners
 registerWebhookListeners(githubWebhooks);
---
+++ apps/agent/src/routes/middleware/error_handler.ts
@@ -0,0 +1,30 @@
+import { Context, Next } from "koa";
+import { ZodError } from "zod";
+import APIError from "~/lib/api_error";
+
+/**
+ * Global error handler
+ */
+export default async function errorHandler(ctx: Context, next: Next) {
+  try {
+    await next();
+  } catch (err) {
+    if (err instanceof ZodError) {
+      console.error(err.issues);
+      // Argument type validation
+      ctx.status = 400;
+      ctx.body = {
+        errors: err.issues,
+        message: err.message,
+      };
+    } else {
+      const typedError = err as APIError;
+      console.error(err);
+      console.error(typedError?.message ?? typedError?.type ?? typedError);
+      ctx.status = typedError?.statusCode ?? 500;
+      ctx.body = {
+        message: typedError?.message ?? typedError?.type ?? "An error occured",
+      };
+    }
+  }
+}
