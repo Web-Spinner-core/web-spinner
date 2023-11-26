45a193f8379597ff228add8d285ea324e178beb7
Gram Liu <gram.liu226@gmail.com>
2023-11-25 20:36:54 -0800

    Add tailwind configs to config package

---
+++ packages/config/tailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "../../packages/ui/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{jsx,tsx}",
+    "./app/**/*.{jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
