173cf3d3fc1795d275eb614733fb3908ea22850a
Gram Liu <gram.liu226@gmail.com>
2023-11-25 22:24:56 -0800

    Add million

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,11 @@
+const million = require("million/compiler");
 /**
  * @type {import('next').NextConfig}
  */
-module.exports = {
-  reactStrictMode: true,
-  transpilePackages: ["@ui"]
-};
+module.exports = million.next(
+  {
+    reactStrictMode: true,
+    transpilePackages: ["@ui"],
+  },
+  { auto: { rsc: true } }
+);
