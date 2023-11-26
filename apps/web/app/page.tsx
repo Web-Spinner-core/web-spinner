0f33aef0a0caa6b5cc8377858c29aaaebe729bdf
Gram Liu <gram.liu226@gmail.com>
2023-11-25 21:58:41 -0800

    Add page headers

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,22 @@
 "use client";
-import { Tldraw } from '@tldraw/tldraw';
-import '@tldraw/tldraw/tldraw.css'
+import '@tldraw/tldraw/tldraw.css';
+import IconLabel from "@ui/components/icon-label";
+import { GitBranchIcon, GithubIcon } from 'lucide-react';
+import NextJsIcon from "@ui/icons/nextjs";
 
+const repo = "Web-Spinner-gramliu/web-spinner";
+const branch = "main"
+const tech = "Next.js App Router";
 
 export default async function IndexPage() {
   return (
-    <div>
-      <h1>tl;draw demo</h1>
-      <div className="h-screen w-screen">
-        <Tldraw />
-      </div>
-    </div>
+    <main className="h-full w-full flex flex-col p-5 pl-10 pt-10">
+      <h1 className="text-3xl font-bold">Web Spinner</h1>
+      <section className="p-4 grid grid-cols-2 grid-rows-2 grid-flow-col">
+        <IconLabel icon={<GithubIcon />} label={repo} />
+        <IconLabel icon={<GitBranchIcon />} label={branch} />
+        <IconLabel icon={<NextJsIcon />} label={tech} />
+      </section>
+    </main>
   );
 }
