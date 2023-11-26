c34933302b7c9032c812baac015f7f4bff690389
Gram Liu <gram.liu226@gmail.com>
2023-11-25 19:17:16 -0800

    Change to default tailwind installation

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,15 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{jsx,tsx}", "./app/**/*.{jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
