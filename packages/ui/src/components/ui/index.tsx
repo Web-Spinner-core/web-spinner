149c08c49bfc268bfed8a26b6406b1158d663bbf
Gram Liu <gram.liu226@gmail.com>
2023-11-26 22:42:45 -0800

    Add shadcn tabs

--- packages/ui/src/components/ui/index.tsx
+++ packages/ui/src/components/ui/index.tsx
@@ -1,5 +1,6 @@
 export * from "./button";
+export * from "./skeleton";
+export * from "./tabs";
 export * from "./toast";
 export * from "./toaster";
 export * from "./use-toast";
-export * from "./skeleton";
