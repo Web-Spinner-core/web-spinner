abdb417ab97b7c397d273aba3c5a9f813b9c15e5
Gram Liu <gram.liu226@gmail.com>
2023-11-26 22:13:47 -0800

    Re-enable backend cache

--- apps/agent/src/pipelines/canvas_to_page/index.ts
+++ apps/agent/src/pipelines/canvas_to_page/index.ts
@@ -46,7 +46,7 @@ export default async function convertCanvasToPage(
       maxTokens: 4096,
       temperature: 0.1,
       callbacks,
-      cache: false
+      cache: true
     });
     const response = await model.call(
       [
