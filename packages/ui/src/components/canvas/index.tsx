f86b8ed950c2221b8c02ac9febaff3082c05a8c2
Gram Liu <gram.liu226@gmail.com>
2023-11-26 16:48:25 -0800

    Refactor Canvas component to handle page changes

--- packages/ui/src/components/canvas/index.tsx
+++ packages/ui/src/components/canvas/index.tsx
@@ -1,22 +1,43 @@
 "use client";
 import { Editor, Tldraw } from "@tldraw/tldraw";
 import "@tldraw/tldraw/tldraw.css";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import initialState from "./initial-state.json";
+
+interface CanvasProps {
+  onPageChanged?: (newPageId: string, oldPageId: string) => void;
+}
 
 /**
  * Canvas wrapper for tldraw
  */
-export default function Canvas() {
+export default function Canvas({
+  onPageChanged
+}: CanvasProps) {
   const [editor, setEditor] = useState<Editor>();
 
-  const onEditorMount = (editor: Editor) => {
+  const onEditorMount = useCallback((editor: Editor) => {
     setEditor(editor);
-  };
+
+    editor.on("change", (change) => {
+      if (change.source !== "user") {
+        return;
+      }
+
+      // Changed page
+      for (const [from, to] of Object.values(change.changes.updated)) {
+        if (from.typeName === "instance" && to.typeName === "instance" && from.currentPageId !== to.currentPageId) {
+          if (onPageChanged) {
+            onPageChanged(to.currentPageId, from.currentPageId);
+          }
+        }
+      }
+    });
+  }, []);
 
   return (
     <Tldraw
-      persistenceKey="doodle-canvas"
+      persistenceKey="web-spinner-canvas"
       onMount={onEditorMount}
       snapshot={initialState}
     />
