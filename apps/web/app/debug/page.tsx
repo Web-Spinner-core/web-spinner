aaf895b4ddfe9c0b6c9eab4f534fb12800fc8466
Gram Liu <gram.liu226@gmail.com>
2023-12-10 16:21:01 -0800

    Add debug page

---
+++ apps/web/app/debug/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+import { useEffect, useRef } from "react";
+import "highlight.js/styles/github.css";
+import { Diff2HtmlUI } from "diff2html/lib-esm/ui/js/diff2html-ui";
+import "diff2html/bundles/css/diff2html.min.css";
+
+const gitDiff = `--- a.tsx
++++ a.tsx
+@@ -0,0 +1,18 @@
++import { ContentArea } from '@/components/ContentArea';
++import { Sidebar } from '@/components/Sidebar';
++import React from "react";
++
++interface DashboardLayoutProps {
++  children: React.ReactNode;
++}
++
++export default function DashboardLayout({ children }: DashboardLayoutProps) {
++  return (
++    <>
++      <Sidebar userName="John Smith" menuItems={[{label: 'Profile'}, {label: 'Settings'}]} />
++      <ContentArea title="Welcome" subtitle="to your private space on the web">
++        {children}
++      </ContentArea>
++    </>
++  );
++}
+--- b.tsx
++++ b.tsx
+@@ -0,0 +1,17 @@
++import { PageLink } from '@/components/PageLink';
++
++export default function DashboardPage() {
++  const pageLinks = [
++    { icon: '⚛️', color: 'text-blue-400', label: 'Physics' },
++    { icon: '🧪', color: 'text-green-400', label: 'Chemistry' },
++    { icon: '🧬', color: 'text-red-400', label: 'Biology' }
++  ];
++
++  return (
++    <>
++      {pageLinks.map((link, index) => (
++        <PageLink key={index} {...link} />
++      ))}
++    </>
++  );
++}`;
+
+export default function DebugPage() {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+    const diff2html = new Diff2HtmlUI(ref.current, gitDiff, {
+      drawFileList: true,
+      fileContentToggle: false,
+      stickyFileHeaders: false,
+    });
+    diff2html.draw();
+    diff2html.highlightCode();
+  }, [ref.current]);
+
+  return (
+    <main>
+      <div ref={ref} />
+    </main>
+  );
+}
